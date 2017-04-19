9f446c1bf018233d2db39aeeb42328be166195d0
Dilson de Jesus Damiao <dilson@cern.ch>
2017-04-19 17:54:22 +0200

    Pixel + Diamond geometry

--- Fireworks/Geometry/python/dumpRecoGeometry_cfg.py
+++ Fireworks/Geometry/python/dumpRecoGeometry_cfg.py
@@ -3,7 +3,7 @@ import sys
 import FWCore.ParameterSet.VarParsing as VarParsing
 from FWCore.Utilities.Enumerate import Enumerate
 
-varType = Enumerate ("Run1 2015 2017 2019 PhaseIPixel 2023Muon SLHC SLHCDB")
+varType = Enumerate ("Run1 2015 2017 2019 2023D7 2023D10 2023D4 2023D8 MaPSA")
 
 def help():
    print "Usage: cmsRun dumpFWRecoGeometry_cfg.py  tag=TAG "
@@ -15,98 +15,105 @@ def help():
    print "       dump in TGeo format to borwse it geomtery viewer"
    print "       import this will in Fireworks with option --sim-geom-file"
    print ""
+   print "   tracker=bool"
+   print "       include Tracker subdetectors"
+   print ""
+   print "   muon=bool"
+   print "       include Muon subdetectors"
+   print ""
+   print "   calo=bool"
+   print "       include Calo subdetectors"
+   print ""
+   print "   timing=bool"
+   print "       include Timing subdetectors"
+   print ""
    print ""
    exit(1);
 
 def recoGeoLoad(score):
-    if score == "Run1CTPPS":
-        print "Loading configuration for tag ", options.tag ,"...\n"
-        #process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-        process.load("Configuration.TotemCommon.geometryRP_real_cfi")
-        process.XMLIdealGeometryESSource.geomXMLFiles.append('Geometry/TotemRPData/data/2015_10_18_fill4511/RP_Dist_Beam_Cent.xml')
+    print "Loading configuration for tag ", options.tag ,"...\n"
 
-    elif score == "Run1":
+    if score == "Run1":
+       process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
        from Configuration.AlCa.autoCond import autoCond
-       process.GlobalTag.globaltag = autoCond['mc']
+       process.GlobalTag.globaltag = autoCond['run1_mc']
        process.load("Configuration.StandardSequences.GeometryDB_cff")
-       process.load("Configuration.StandardSequences.Reconstruction_cff")
-
+       
     elif score == "2015":
+       process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+       from Configuration.AlCa.autoCond import autoCond
+       process.GlobalTag.globaltag = autoCond['run2_mc']
+       process.load("Configuration.StandardSequences.GeometryDB_cff")
+       
+    elif score == "2017":
+       process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+       from Configuration.AlCa.autoCond import autoCond
+       process.GlobalTag.globaltag = autoCond['upgrade2017']
+       process.load('Configuration.Geometry.GeometryExtended2017Reco_cff')
+       
+    elif  score == "2019":
+       process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+       from Configuration.AlCa.autoCond import autoCond
+       process.GlobalTag.globaltag = autoCond['upgrade2019']
+       ## NOTE: There is no PTrackerParameters Rcd in this GT yet
+       process.load('Geometry.TrackerGeometryBuilder.trackerParameters_cfi')
+       process.load('Configuration.Geometry.GeometryExtended2019Reco_cff')
+       ## NOTE: There are no Muon alignement records in the GT yet
+       process.DTGeometryESModule.applyAlignment = cms.bool(False)
+       process.CSCGeometryESModule.applyAlignment = cms.bool(False)
+       
+    elif  score == "2023D7":
+       process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+       from Configuration.AlCa.autoCond import autoCond
+       process.GlobalTag.globaltag = autoCond['run2_mc']
+       process.load('Configuration.Geometry.GeometryExtended2023D7Reco_cff')
+       
+    elif  score == "2023D10":
+       process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+       from Configuration.AlCa.autoCond import autoCond
+       process.GlobalTag.globaltag = autoCond['phase2_realistic']
+       process.load('Configuration.Geometry.GeometryExtended2023D10Reco_cff')
+       
+    elif  score == "2023D4":
+       process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+       from Configuration.AlCa.autoCond import autoCond
+       process.GlobalTag.globaltag = autoCond['phase2_realistic']
+       process.load('Configuration.Geometry.GeometryExtended2023D4Reco_cff')
+
+    elif  score == "2023D8":
+       process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+       from Configuration.AlCa.autoCond import autoCond
+       process.GlobalTag.globaltag = autoCond['phase2_realistic']
+       process.load('Configuration.Geometry.GeometryExtended2023D8Reco_cff')
+       
+    elif score == "MaPSA":
+       process.load('Geometry.TrackerGeometryBuilder.idealForDigiTrackerGeometry_cff')
+       process.load('Geometry.TrackerCommonData.mapsaGeometryXML_cfi')
+       process.load('Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi')
+       process.load('Geometry.TrackerNumberingBuilder.trackerTopology_cfi')
+       process.load('Geometry.TrackerGeometryBuilder.trackerParameters_cfi')
+       process.load('Geometry.TrackerGeometryBuilder.trackerGeometry_cfi')
+       process.trackerGeometry.applyAlignment = cms.bool(False)
+       process.load('RecoTracker.GeometryESProducer.TrackerRecoGeometryESProducer_cfi')
+
+       process.load('Geometry.CommonDetUnit.bareGlobalTrackingGeometry_cfi')
+       
+    elif score == "HGCTB160": ## hgcal testbeam
+       process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff") 
        from Configuration.AlCa.autoCond import autoCond
        process.GlobalTag.globaltag = autoCond['mc']
-       process.load("Configuration.Geometry.GeometryExtended2015Reco_cff");
-
-    elif score == "2017":
-       from Configuration.AlCa.GlobalTag import GlobalTag
-       process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgrade2017', '')
-       process.load('Configuration.Geometry.GeometryExtended2017Reco_cff')
-       # Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.combinedCustoms
-       #from SLHCUpgradeSimulations.Configuration.combinedCustoms import cust_2017 
-       #process.load("SLHCUpgradeSimulations.Configuration.combinedCustoms.cust_2017")
-      # process = cust_2017(process)
-
-    elif  score == "2019":
-      from Configuration.AlCa.GlobalTag import GlobalTag
-      process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgrade2019', '')
-      process.load('Configuration.Geometry.GeometryExtended2019Reco_cff')
-      #from SLHCUpgradeSimulations.Configuration.combinedCustoms import cust_2019
-
-    elif score ==  "PhaseIPixel":
-      from Configuration.AlCa.GlobalTag import GlobalTag
-      process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:mc', '')
-      process.load('Configuration.Geometry.GeometryExtendedPhaseIPixelReco_cff')
-
-    elif  score == "2023":
-      from Configuration.AlCa.autoCond import autoCond
-      process.GlobalTag.globaltag = autoCond['mc']
-      #from Configuration.AlCa.GlobalTag import GlobalTag
-      #process.GlobalTag = GlobalTag(process.GlobalTag, 'PH2_1K_FB_V6::All', '')
-      process.load('Configuration.Geometry.GeometryExtended2023Reco_cff')
-      #from SLHCUpgradeSimulations.Configuration.combinedCustoms import cust_2019
-      #process = cust_2019(process)
-      
-    elif  score == "2023Muon":
-      from Configuration.AlCa.autoCond import autoCond
-      process.GlobalTag.globaltag = autoCond['mc']
-      #from Configuration.AlCa.GlobalTag import GlobalTag
-      #process.GlobalTag = GlobalTag(process.GlobalTag, 'PH2_1K_FB_V6::All', '')
-      process.load('Configuration.Geometry.GeometryExtended2023MuonReco_cff')
-      # Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.combinedCustoms
-      #from SLHCUpgradeSimulations.Configuration.combinedCustoms import cust_2023Muon
-      #call to customisation function cust_2023Muon imported from SLHCUpgradeSimulations.Configuration.combinedCustoms
-      #process = cust_2023Muon(process)
-
-    elif  score == "GEMDev":
-      from Configuration.AlCa.autoCond import autoCond
-      process.GlobalTag.globaltag = autoCond['mc']
-      process.load('Configuration.Geometry.GeometryExtended2015MuonGEMDevReco_cff')
-
-    elif score == "SLHCDB": # orig dumpFWRecoSLHCGeometry_cfg.py
-      process.GlobalTag.globaltag = 'DESIGN42_V17::All'
-      process.load("Configuration.StandardSequences.GeometryDB_cff")
-      process.load("Configuration.StandardSequences.Reconstruction_cff")
-
-      process.GlobalTag.toGet = cms.VPSet(
-        cms.PSet(record = cms.string("GeometryFileRcd"),
-                 tag = cms.string("XMLFILE_Geometry_428SLHCYV0_Phase1_R30F12_HCal_Ideal_mc"),
-                 connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_42X_GEOMETRY")
-                 ),
-        cms.PSet(record = cms.string('IdealGeometryRecord'),
-                 tag = cms.string('TKRECO_Geometry_428SLHCYV0'),
-                 connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_42X_GEOMETRY")),
-        cms.PSet(record = cms.string('PGeometricDetExtraRcd'),
-                 tag = cms.string('TKExtra_Geometry_428SLHCYV0'),
-                 connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_42X_GEOMETRY")),
-                 )
-
-    elif score == varType.valueForKey("SLHC"): # orig dumpFWRecoGeometrySLHC_cfg.py
-      from Configuration.AlCa.autoCond import autoCond
-      process.GlobalTag.globaltag = autoCond['mc']
-      process.load("Configuration.Geometry.GeometrySLHCSimIdeal_cff")
-      process.load("Configuration.Geometry.GeometrySLHCReco_cff")
-      process.load("Configuration.StandardSequences.Reconstruction_cff")
-      process.trackerSLHCGeometry.applyAlignment = False
-
+       process.load('Geometry.HGCalCommonData.hgcalV6ParametersInitialization_cfi')
+       process.load('Geometry.HGCalCommonData.hgcalV6NumberingInitialization_cfi')
+       process.load('Geometry.CaloEventSetup.HGCalV6Topology_cfi')
+       process.load('Geometry.HGCalGeometry.HGCalV6GeometryESProducer_cfi')
+       process.load('Geometry.CaloEventSetup.CaloTopology_cfi')
+       process.load('Geometry.CaloEventSetup.CaloGeometryBuilder_cfi')
+       process.CaloGeometryBuilder = cms.ESProducer(
+          "CaloGeometryBuilder",
+          SelectedCalos = cms.vstring("HGCalEESensitive")
+       )
+       process.load("SimG4CMS.HGCalTestBeam.HGCalTB160XML_cfi")
+       
     else:
       help()
 
@@ -124,13 +131,35 @@ options.register ('tag',
                   VarParsing.VarParsing.varType.string,
                   "tag info about geometry database conditions")
 
-
 options.register ('tgeo',
                   False, # default value
                   VarParsing.VarParsing.multiplicity.singleton,
                   VarParsing.VarParsing.varType.bool,
                   "write geometry in TGeo format")
 
+options.register ('tracker',
+                  True, # default value
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.bool,
+                  "write Tracker geometry")
+
+options.register ('muon',
+                  True, # default value
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.bool,
+                  "write Muon geometry")
+
+options.register ('calo',
+                  True, # default value
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.bool,
+                  "write Calo geometry")
+
+options.register ('timing',
+                  False, # default value
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.bool,
+                  "write Timing geometry")
 
 options.register ('out',
                   defaultOutputFileName, # default value
@@ -154,7 +183,11 @@ recoGeoLoad(options.tag)
 if ( options.tgeo == True):
     if (options.out == defaultOutputFileName ):
         options.out = "cmsTGeoRecoGeom-" +  str(options.tag) + ".root"
-    process.add_(cms.ESProducer("FWTGeoRecoGeometryESProducer"))
+    process.add_(cms.ESProducer("FWTGeoRecoGeometryESProducer",
+                 Tracker = cms.untracked.bool(options.tracker),
+                 Muon = cms.untracked.bool(options.muon),
+                 Calo = cms.untracked.bool(options.calo),
+                 Timing = cms.untracked.bool(options.timing)))
     process.dump = cms.EDAnalyzer("DumpFWTGeoRecoGeometry",
                               tagInfo = cms.untracked.string(options.tag),
                        outputFileName = cms.untracked.string(options.out)
@@ -162,7 +195,11 @@ if ( options.tgeo == True):
 else:
     if (options.out == defaultOutputFileName ):
         options.out = "cmsRecoGeom-" +  str(options.tag) + ".root"
-    process.add_(cms.ESProducer("FWRecoGeometryESProducer"))
+    process.add_(cms.ESProducer("FWRecoGeometryESProducer",
+                 Tracker = cms.untracked.bool(options.tracker),
+                 Muon = cms.untracked.bool(options.muon),
+                 Calo = cms.untracked.bool(options.calo),
+                 Timing = cms.untracked.bool(options.timing)))
     process.dump = cms.EDAnalyzer("DumpFWRecoGeometry",
                               level   = cms.untracked.int32(1),
                               tagInfo = cms.untracked.string(options.tag),
@@ -171,3 +208,5 @@ else:
 
 print "Dumping geometry in " , options.out, "\n"; 
 process.p = cms.Path(process.dump)
+
+
